1a5f10532b7ee24a2ac5538ad9d1cbe87ffd601d
Xuwang Yin <xuwangyin@gmail.com>
2020-01-26 16:31:46 -0500

    update results

--- train.py
+++ train.py
@@ -14,12 +14,21 @@ import matplotlib.pyplot as plt
 import argparse
 from pathlib import Path
 from sklearn.metrics import confusion_matrix
+from torch.optim.lr_scheduler import StepLR
+from IPython.core.debugger import set_trace
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', default=32, type=int)
-parser.add_argument('--datadir', default='data700', type=str)
-parser.add_argument('--img_size', default=28, type=int)
+parser.add_argument('--batch_size', default=64, type=int)
+parser.add_argument('--dataset', default='data701', type=str, choices=['data701', 'data700'])
+parser.add_argument('--img_size', default=84, type=int)
+parser.add_argument('--epochs', default=50, type=int)
 args = parser.parse_args()
+
+if args.dataset == 'data700':
+    args.img_size = 28
+if args.dataset == 'data701':
+    args.img_size = 84
 
 np.random.seed(0)
 torch.manual_seed(0)
@@ -29,13 +38,16 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class MNISTNet(nn.Module):
-    def __init__(self):
-        super(MNISTNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, 1)
+    def __init__(self, input_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
         self.dropout1 = nn.Dropout2d(0.25)
         self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216, 128)
+        if args.img_size == 28:
+            self.fc1 = nn.Linear(9216, 128)
+        if args.img_size == 84:
+            self.fc1 = nn.Linear(64*40*40, 128)
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
@@ -54,7 +66,7 @@ class MNISTNet(nn.Module):
 
 @lru_cache()
 def load_data():
-    cache_file = os.path.join(args.datadir, 'customizedAffNIST.npz')
+    cache_file = os.path.join(args.dataset, 'customizedAffNIST.npz')
     if os.path.exists(cache_file):
         data = np.load(cache_file)
         return (data['x_train'], data['y_train']), (data['x_test'], data['y_test'])
@@ -62,7 +74,7 @@ def load_data():
     x_train, y_train, x_test, y_test = [], [], [], []
     for split in ['training', 'testing']:
         for classidx in range(10):
-            datafile = os.path.join(args.datadir, '{}/dataORG_{}.mat'.format(split, classidx))
+            datafile = os.path.join(args.dataset, '{}/dataORG_{}.mat'.format(split, classidx))
             data = loadmat(datafile)['xxO'].transpose([2, 0, 1])
             label = np.zeros(data.shape[0], dtype=np.int64)+classidx
             if split == 'training':
@@ -94,13 +106,19 @@ if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = load_data()
     # x_train shape: (class_idx*n_samples, args.img_size, args.img_size)
     x_train = (x_train.astype(np.float32) / 255. - 0.5) / 0.5
-    x_train_format = np.repeat(x_train.reshape(-1, 1, args.img_size, args.img_size), axis=1, repeats=3)
+    x_train = x_train.reshape(-1, 1, args.img_size, args.img_size)
+
+    x_train_format = np.repeat(x_train, axis=1, repeats=3)
 
     x_test = (x_test.astype(np.float32) / 255. - 0.5) / 0.5
-    x_test_format = np.repeat(x_test.reshape(-1, 1, args.img_size, args.img_size), axis=1, repeats=3)
+    x_test = x_test.reshape(-1, 1, args.img_size, args.img_size)
+
+    x_test_format = np.repeat(x_test, axis=1, repeats=3)
     x_test_format = torch.from_numpy(x_test_format).to(device)
 
-    indices = loadmat(os.path.join(args.datadir, 'Ind_tr.mat'))['indtr'] - 1 # index start from 1
+    indices = loadmat(os.path.join(args.dataset, 'Ind_tr.mat'))['indtr'] - 1 # index start from 1
+    print('index file: {}'.format(indices.shape))
+
     val_indices = indices[4096:, 0]
     x_val = take_samples(x_train_format, val_indices)
     x_val = torch.from_numpy(x_val).to(device)
@@ -108,34 +126,38 @@ if __name__ == '__main__':
     print('x_val.shape {}'.format(x_val.shape))
 
     # model = models.vgg11_bn(num_classes=10).to(device)
-    model = MNISTNet().to(device)
+    model = MNISTNet(input_channels=3).to(device)
     torch.save(model.state_dict(), './model_init.pth')
 
     for n_samples in [2**i for i in range(13)]:
-    # for n_samples in [500]:
-        for run in range(5):
+    # for n_samples in [1]:
+        for run in range(1):
             print('============== num samples {} run {} ============'.format(n_samples, run))
             model.load_state_dict(torch.load('./model_init.pth'))
 
             sub_index = indices[:n_samples, run]
             x_train_sub = take_samples(x_train_format, sub_index)
+            assert n_samples <= indices.shape[0]
             y_train_sub = np.repeat(np.arange(10), n_samples)
 
             criterion = nn.CrossEntropyLoss()
             # optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9, weight_decay=5e-4)
             optimizer = optim.Adam(model.parameters(), lr=5e-4)
+            # optimizer = optim.Adadelta(model.parameters(), lr=1.0)
+            # scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
 
-            save_path = 'results/{}/samples-{}-model-{}/'.format(args.datadir, n_samples, type(model).__name__)
+            save_path = 'results/{}/samples-{}-model-{}/'.format(args.dataset, n_samples, type(model).__name__)
             Path(save_path).mkdir(parents=True, exist_ok=True)
             ckpt_path = os.path.join(save_path, 'run{}.pkl'.format(run))
             best_val_acc = 0.0
 
-            for epoch in range(50):
+            for epoch in range(args.epochs):
                 perm = np.random.permutation(x_train_sub.shape[0])
                 x_train_sub_perm = x_train_sub[perm]
                 y_train_sub_perm = y_train_sub[perm]
 
                 # train
+                model.train()
                 for i in range(0, x_train_sub_perm.shape[0], args.batch_size):
                     inputs = x_train_sub_perm[i: i + args.batch_size]
                     targets = y_train_sub_perm[i: i + args.batch_size]
@@ -156,7 +178,9 @@ if __name__ == '__main__':
                     if (i//args.batch_size) // 10 == 0:
                         print('epoch {} iter {} train loss {:.5f} acc {:.5f}'.format(epoch, i//args.batch_size, loss.item(), train_acc))
 
+                # scheduler.step()
                 # validation
+                model.eval()
                 with torch.no_grad():
                     val_logits = []
                     for i in range(0, x_val.shape[0], 2000):
@@ -171,6 +195,7 @@ if __name__ == '__main__':
                         torch.save(state, ckpt_path)
                         print('saved to ' + ckpt_path)
             # test
+            model.eval()
             with torch.no_grad():
                 state = torch.load(ckpt_path)
                 model.load_state_dict(state['model'])
