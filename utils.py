4b01524514732b4374092ea8280f87d533a2f2be
Xuwang Yin <xuwangyin@gmail.com>
2020-03-20 17:12:40 -0400

    add support Synthetic100

--- utils.py
+++ utils.py
@@ -136,7 +136,7 @@ def take_train_val_samples(x_train, y_train, n_samples_perclass, num_classes, re
 
 
 def dataset_config(dataset):
-    assert dataset in ['OASIS1_age', 'HEP2', 'AffMNIST', 'LiverN', 'MNIST', 'OAM', 'OAM_t5', 'OAM_t10', 'SignMNIST', 'Synthetic', 'CIFAR10', 'MNIST_outDist']
+    assert dataset in ['OASIS1_age', 'HEP2', 'AffMNIST', 'LiverN', 'MNIST', 'OAM', 'OAM_t5', 'OAM_t10', 'SignMNIST', 'Synthetic', 'Synthetic100', 'CIFAR10', 'MNIST_outDist']
     if dataset in ['MNIST']:
         rm_edge = True
         num_classes = 10
@@ -187,5 +187,10 @@ def dataset_config(dataset):
         num_classes = 2
         img_size = 208
         po_train_max = 6
+    elif dataset in ['Synthetic100']:
+        rm_edge = True
+        num_classes = 100
+        img_size = 64
+        po_train_max = 6  # maximum train samples = 2^po_max
 
     return num_classes, img_size, po_train_max, rm_edge
