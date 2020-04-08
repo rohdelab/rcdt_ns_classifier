459556b207671e339aae8ff6be2f45a6ea4fe74b
ar3fx <ar3fx@d-172-18-101-57.vpn.net.virginia.edu>
2020-04-08 18:45:24 -0400

    Updated the shell script

--- reproduce_paper_results/MNIST_classification.sh
+++ reproduce_paper_results/MNIST_classification.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # RCDT-SUBS classification on MNIST dataset
-python RCDTSUBS_classification.py --dataset MNIST
+python RCDT_NS_classification.py --dataset MNIST
 
 # Use shallowcnn model to do classification on MNIST dataset
 python CNN_classification.py --dataset MNIST --model shallowcnn
